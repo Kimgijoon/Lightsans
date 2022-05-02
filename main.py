b25bb9a57e1e58292eb53b1cd3a0d6e7966d4a93
Kimgijoon <kozx1212@gmail.com>
2022-05-02 21:26:38 +0900

    feat: lightsans 모델 학습 수행 2022.05.02 정의된 모델을 main에서 학습할 수 있도록 해놓음

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import json
 import pickle
+from datetime import datetime
 
+import numpy as np
 import tensorflow as tf
 
 from utils.create_tfrecord import save_tfrecords
+from src.recommender import SequentialRecommender
 from utils.data_util import load_interaction, split_data, chunk_data, get_features_from_tfrecord
+
 
 flags = tf.compat.v1.flags
 FLAGS = flags.FLAGS
@@ -14,9 +18,16 @@ flags.mark_flag_as_required('op')
 
 flags.DEFINE_string('data_dir', 'data', 'Path to input directory')
 flags.DEFINE_string('config_dir', 'configs', 'Directory of config file')
+flags.DEFINE_string('checkpoint_dir', 'checkpoints', 'Directory of model checkpoints')
+flags.DEFINE_string('logs_dir', 'logs', 'Directory with tensorboard log')
 flags.DEFINE_string('dataset', 'ml-100k.inter', 'Raw data filename')
 flags.DEFINE_integer('chunk_size', 10, 'The number of tfrecord dataset i.e. raw data divice to tfrecord of size N')
 flags.DEFINE_string('split_ratio', '0.2, 0.1', 'Valid, Test ratio in dataset')
+flags.DEFINE_integer('batch_size', 512, 'Train batch size')
+flags.DEFINE_integer('epochs', 49, 'Train epoch')
+flags.DEFINE_integer('save_period', 5, 'Save checkpoint per step')
+flags.DEFINE_integer('topk', 1, 'Metric topk')
+flags.DEFINE_boolean('use_mp', False, 'Flags that determine whether to use mixed precision')
 
 
 def main(_):
@@ -63,7 +74,32 @@ def main(_):
         with open(f'{FLAGS.config_dir}/config.json', 'w') as f:
             f.write(json.dumps(configs, ensure_ascii=False))
     elif FLAGS.op == 'train':
-        pass
+        feature_description = dict(
+            user_id=tf.io.FixedLenFeature([1], tf.int64),
+            sequence=tf.io.FixedLenFeature([1, 200], tf.int64),
+            sequence_length=tf.io.FixedLenFeature([1], tf.int64),
+            label=tf.io.FixedLenFeature(configs['n_items'], tf.int64)
+        )
+        trainset = get_features_from_tfrecord(feature_description,
+                                             f'{FLAGS.data_dir}/prep',
+                                             batch_size=FLAGS.batch_size,
+                                             shuffle_size=configs.get('train'),
+                                             data_type='train')
+        validset = get_features_from_tfrecord(feature_description,
+                                             f'{FLAGS.data_dir}/prep',
+                                             batch_size=FLAGS.batch_size,
+                                             shuffle_size=configs.get('valid'),
+                                             data_type='valid')
+
+        lightsans = SequentialRecommender(configs,
+                                          batch_size=FLAGS.batch_size,
+                                          epochs=FLAGS.epochs,
+                                          is_training=True,
+                                          topk=1)
+        current_time = datetime.now().strftime("%Y%m%d-%H%M%S")
+        ckpt_dir = f'{FLAGS.checkpoint_dir}/{current_time}'
+        logs_dir = f'{FLAGS.logs_dir}/{current_time}'
+        lightsans.train(trainset, validset, ckpt_dir, FLAGS.save_period, logs_dir)
     else:
         pass
 
